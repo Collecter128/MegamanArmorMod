30f603143e068970430a658a85fbd804531b0261
Collecter128 <45398071+Collecter128@users.noreply.github.com>
2022-03-15 10:07:53 -0400

    Add files via upload

---
+++ crafting/DyeArmorColorizer.java
@@ -0,0 +1,108 @@
+package com.collecter128.megamanarmormod.items.crafting;
+
+import java.util.List;
+
+import com.collecter128.megamanarmormod.core.Imorecolorfularmor;
+import com.collecter128.megamanarmormod.items.ArmorColorizerCustom;
+import com.google.common.collect.Lists;
+
+import net.minecraft.inventory.CraftingInventory;
+import net.minecraft.item.DyeItem;
+import net.minecraft.item.IDyeableArmorItem;
+//import net.minecraft.item.IDyeableArmorItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.ArmorDyeRecipe;
+import net.minecraft.item.crafting.IRecipeSerializer;
+import net.minecraft.item.crafting.SpecialRecipe;
+import net.minecraft.item.crafting.SpecialRecipeSerializer;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.World;
+
+public class DyeArmorColorizer extends SpecialRecipe {//extends ArmorDyeRecipe
+	public DyeArmorColorizer(ResourceLocation idIn) {
+	      super(idIn);
+	   }
+	
+	
+//	public ArmorDyeRecipe(ResourceLocation idIn) {
+//	      super(idIn);
+//	   }
+	   SpecialRecipeSerializer<DyeArmorColorizer> CRAFTING_SPECIAL_ARMORCOLORIZERDYE = IRecipeSerializer.register("crafting_special_armorcolorizerdye", new SpecialRecipeSerializer<>(DyeArmorColorizer::new));
+	   /**
+	    * Used to check if a recipe matches current crafting inventory
+	    */
+	   @Override
+	   public boolean matches(CraftingInventory inv, World worldIn) {
+	      ItemStack itemstack = ItemStack.EMPTY;
+	      List<ItemStack> list = Lists.newArrayList();
+
+	      for(int i = 0; i < inv.getSizeInventory(); ++i) {
+	         ItemStack itemstack1 = inv.getStackInSlot(i);
+	         if (!itemstack1.isEmpty()) {
+	            if (itemstack1.getItem() instanceof ArmorColorizerCustom) {
+	               if (!itemstack.isEmpty()) {
+	                  return false;
+	               }
+
+	               itemstack = itemstack1;
+	            } else {
+	               if (!(itemstack1.getItem() instanceof DyeItem)) {
+	                  return false;
+	               }
+
+	               list.add(itemstack1);
+	            }
+	         }
+	      }
+
+	      return !itemstack.isEmpty() && !list.isEmpty();
+	   }
+
+	   /**
+	    * Returns an Item that is the result of this recipe
+	    */
+	   public ItemStack getCraftingResult(CraftingInventory inv) {
+	      List<DyeItem> list = Lists.newArrayList();
+	      ItemStack itemstack = ItemStack.EMPTY;
+
+	      for(int i = 0; i < inv.getSizeInventory(); ++i) {
+	         ItemStack itemstack1 = inv.getStackInSlot(i);
+	         if (!itemstack1.isEmpty()) {
+	            Item item = itemstack1.getItem();
+	            if (item instanceof ArmorColorizerCustom) {//Imorecolorfularmor
+	               if (!itemstack.isEmpty()) {
+	                  return ItemStack.EMPTY;
+	               }
+
+	               itemstack = itemstack1.copy();
+	            } else {
+	               if (!(item instanceof DyeItem)) {
+	                  return ItemStack.EMPTY;
+	               }
+
+	               list.add((DyeItem)item);
+	            }
+	         }
+	      }
+
+	      return !itemstack.isEmpty() && !list.isEmpty() ? IDyeableArmorItem.dyeItem(itemstack, list) : ItemStack.EMPTY;
+	   }
+	   
+//	   public ItemStack customitemcolor(ItemStack item) {
+//		   IDyeableArmorItem.dyeItem(itemstack, list)
+//		   return item;
+//	   }
+
+	   /**
+	    * Used to determine if this recipe can fit in a grid of the given width/height
+	    */
+	   public boolean canFit(int width, int height) {
+	      return width * height >= 2;
+	   }
+
+	   @Override
+	   public IRecipeSerializer<?> getSerializer() {
+	      return CRAFTING_SPECIAL_ARMORCOLORIZERDYE;
+	   }
+}
