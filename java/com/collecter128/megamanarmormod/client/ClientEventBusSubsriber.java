a1a36a43963acd52cad6faff864fa2c875d320e4
Collecter128 <45398071+Collecter128@users.noreply.github.com>
2023-11-14 09:28:08 -0500

    Add files via upload

--- java/com/collecter128/megamanarmormod/client/ClientEventBusSubsriber.java
+++ java/com/collecter128/megamanarmormod/client/ClientEventBusSubsriber.java
@@ -3,6 +3,7 @@ package com.collecter128.megamanarmormod.client;
 import java.awt.event.KeyEvent;
 
 import com.collecter128.megamanarmormod.client.entity.bustershotentityrenderer;
+import com.collecter128.megamanarmormod.core.CustomItemColorsNew;
 import com.collecter128.megamanarmormod.core.EntityTypesInit;
 import com.collecter128.megamanarmormod.core.KeybindInit;
 
@@ -13,6 +14,7 @@ import net.minecraftforge.fml.client.registry.RenderingRegistry;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod.EventBusSubscriber(modid = "megamanarmormod", bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public class ClientEventBusSubsriber {
@@ -22,6 +24,7 @@ public class ClientEventBusSubsriber {
 		KeybindInit.register(event);
 		
 		RenderingRegistry.registerEntityRenderingHandler(EntityTypesInit.BusterShot.get(), bustershotentityrenderer::new);
+		FMLJavaModLoadingContext.get().getModEventBus().addListener(CustomItemColorsNew::registerItemColor);
 		
 	}
 }
