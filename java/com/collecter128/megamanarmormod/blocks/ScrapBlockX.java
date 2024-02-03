b2bb0483ac236e35dcd6be43919480c9ef8602e3
Collecter128 <45398071+Collecter128@users.noreply.github.com>
2024-02-02 19:39:15 -0500

    Add files via upload
    Added new Boss armor crafting items to blocks

--- java/com/collecter128/megamanarmormod/blocks/ScrapBlockX.java
+++ java/com/collecter128/megamanarmormod/blocks/ScrapBlockX.java
@@ -11,7 +11,6 @@ import net.minecraft.block.BlockState;
 import net.minecraft.block.BreakableBlock;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.Direction;
 import net.minecraft.util.SoundCategory;
@@ -23,9 +22,9 @@ import net.minecraft.world.World;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 
-public class BlueMysteryDataBlock extends BreakableBlock{
+public class ScrapBlockX extends BreakableBlock{
 	
-	   public BlueMysteryDataBlock(Properties properties) {
+	   public ScrapBlockX(Properties properties) {
 		super(properties);
 	   }
 
@@ -36,40 +35,31 @@ public class BlueMysteryDataBlock extends BreakableBlock{
 	   public void playerDestroy(World worldIn, PlayerEntity player, BlockPos pos, BlockState state, @Nullable TileEntity p_180657_5_, ItemStack p_180657_6_) {
 	   //public void onRemove(World worldIn, BlockPos pos, BlockState state, PlayerEntity player) {
 	      if (!worldIn.isClientSide) {
-	    	 ItemStack itemstackIn = new ItemStack(ItemInit.NormalData.get());
+	    	 ItemStack itemstackIn = new ItemStack(ItemInit.SigmaVirus.get());
 	    	 Random random = new Random();
-	    	 int RandomInt = random.nextInt(7);
+	    	 int RandomInt = random.nextInt(4);
 	    	 
 	    	 switch(RandomInt) {
 	    	 	case 0: 
-	    	 		itemstackIn = new ItemStack(ItemInit.BusterData.get());
+	    	 		itemstackIn = new ItemStack(ItemInit.SigmaVirus.get());
 	    	 		break;
 	    	 	case 1: 
-	    	 		itemstackIn = new ItemStack(ItemInit.SwordData.get());
+	    	 		itemstackIn = new ItemStack(ItemInit.FakeZeroPiece1.get());
 	    	 		break;
 	    	 	case 2: 
-	    	 		itemstackIn = new ItemStack(ItemInit.NormalData.get());
+	    	 		itemstackIn = new ItemStack(ItemInit.FakeZeroPiece2.get());
 	    	 		break;
 	    	 	case 3: 
-	    	 		itemstackIn = new ItemStack(ItemInit.DarkData.get());
-	    	 		break;
-	    	 	case 4: 
-	    	 		itemstackIn = new ItemStack(ItemInit.RecoverData.get());
-	    	 		break;
-	    	 	case 5: 
-	    	 		itemstackIn = new ItemStack(ItemInit.PanelData.get());
-	    	 		break;
-	    	 	case 6: 
-	    	 		itemstackIn = new ItemStack(ItemInit.VirusData.get());
+	    	 		itemstackIn = new ItemStack(ItemInit.FakeZeroPiece3.get());
 	    	 		break;
 	    	 	
 	    	 		
 	    	 	default:
-	    	 		itemstackIn = new ItemStack(ItemInit.BusterData.get());
+	    	 		itemstackIn = new ItemStack(ItemInit.SigmaVirus.get());
 	    	 }
 	    	  
-	    	 worldIn.playSound((PlayerEntity)null, pos, SoundEvents.FIREWORK_ROCKET_LAUNCH, SoundCategory.BLOCKS, 1.0F, 0.8F + worldIn.random.nextFloat() * 0.4F);
-	         //player.inventory.add(itemstackIn);
+	    	 worldIn.playSound((PlayerEntity)null, pos, SoundEvents.METAL_BREAK, SoundCategory.BLOCKS, 1.0F, 0.8F + worldIn.random.nextFloat() * 0.4F);
+	    	//player.inventory.add(itemstackIn);
 	         popResource(worldIn, pos, itemstackIn);
 	    	 //player.inventory.addItemStackToInventory(itemstackIn);
 	      }
