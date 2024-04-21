a4169a96fbdd51563d6cbe38ef7eea66ad6d004d
Collecter128 <45398071+Collecter128@users.noreply.github.com>
2024-04-21 10:21:21 -0400

    Add files via upload

---
+++ java/com/collecter128/megamanarmormod/items/BitArmor.java
@@ -0,0 +1,209 @@
+package com.collecter128.megamanarmormod.items;
+
+import com.collecter128.megamanarmormod.client.models.bit;
+import com.collecter128.megamanarmormod.client.models.bit_leggings;
+
+import net.minecraft.client.renderer.entity.model.BipedModel;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.ArmorItem;
+import net.minecraft.item.IArmorMaterial;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.CompoundNBT;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+
+public class BitArmor extends ArmorItem{
+	
+	public int MainColorDefault = 16634376;//Main gold
+	public int SecondaryColorDefault = 16615944;//Secondary orange
+	public int ThirdColorDefault = 14682880; //red
+	public int WhiteColorDefault = 16777215;//Basic White
+	public int GlowyColorDefault = 16714250;//Glow? red
+	public int GrayColorDefault = 12434877;//gray
+	public int FourthColorDefault = 3376639; //blue
+	public int FifthColorDefault = 6411749; //light blue
+	public int MainColor = MainColorDefault;
+	public int SecondaryColor = SecondaryColorDefault;
+	public int ThirdColor = ThirdColorDefault;
+	public int WhiteColor = WhiteColorDefault;
+	public int GlowyColor = GlowyColorDefault;
+	public int GrayColor = GrayColorDefault;
+	public int FourthColor = FourthColorDefault;
+	public int FifthColor = FifthColorDefault;
+
+	public BitArmor(IArmorMaterial materialIn, EquipmentSlotType slot, Properties builderIn) {
+		super(materialIn, slot, builderIn);
+		
+	}
+	@OnlyIn(Dist.CLIENT)
+	@Override
+	public <A extends BipedModel<?>> A getArmorModel(LivingEntity entityLiving, ItemStack itemStack, EquipmentSlotType armorSlot, A _default) {
+		
+		if(armorSlot != EquipmentSlotType.LEGS) {
+			bit model = new bit(1.0f, armorSlot);
+		model.hat.visible = armorSlot == EquipmentSlotType.HEAD;
+
+		model.young = _default.young;
+        model.crouching = _default.crouching;
+        model.riding = _default.riding;
+        model.rightArmPose = _default.rightArmPose;
+        model.leftArmPose = _default.leftArmPose;
+        
+        CompoundNBT compoundnbt = itemStack.getTagElement("displaymm");
+
+	      if(compoundnbt != null && compoundnbt.contains("MainColor", 99)) {
+	    	  //CompoundNBT maincompund = itemStack.getChildTag("MainColor");
+	    	  //armorcolorer = compoundnbt.getInt("MainColor");
+	    	  model.MainColor = compoundnbt.getInt("MainColor");
+
+	      }
+	      if(compoundnbt != null && compoundnbt.contains("SecondaryColor", 99)) {
+
+	    	  model.SecondaryColor = compoundnbt.getInt("SecondaryColor");
+
+	      }
+	      
+	      if(compoundnbt != null && compoundnbt.contains("WhiteColor", 99)) {
+
+	    	  model.WhiteColor = compoundnbt.getInt("WhiteColor");
+
+	      }
+	      
+	      if(compoundnbt != null && compoundnbt.contains("ThirdColor", 99)) {
+
+	    	  model.ThirdColor = compoundnbt.getInt("ThirdColor");
+
+	      }
+	      
+	      if(compoundnbt != null && compoundnbt.contains("GlowyColor", 99)) {
+
+	    	  model.GlowyColor = compoundnbt.getInt("GlowyColor");
+
+	      }
+	      
+	      if(compoundnbt != null && compoundnbt.contains("GrayColor", 99)) {
+
+	    	  model.GrayColor = compoundnbt.getInt("GrayColor");
+
+	      }
+	      if(compoundnbt != null && compoundnbt.contains("FourthColor", 99)) {
+
+	    	  model.FourthColor = compoundnbt.getInt("FourthColor");
+
+	      }
+	      if(compoundnbt != null && compoundnbt.contains("FifthColor", 99)) {
+
+	    	  model.FifthColor = compoundnbt.getInt("FifthColor");
+
+	      }
+      
+		this.MainColor = model.MainColor;
+		this.SecondaryColor = model.SecondaryColor;
+		this.WhiteColor = model.WhiteColor;
+		this.ThirdColor = model.ThirdColor;
+		this.GlowyColor = model.GlowyColor;
+		this.GrayColor = model.GrayColor;
+
+        return (A) model;
+		}
+		else {
+			bit_leggings model = new bit_leggings(1.0f);
+			model.hat.visible = armorSlot == EquipmentSlotType.HEAD;
+
+			model.young = _default.young;
+	        model.crouching = _default.crouching;
+	        model.riding = _default.riding;
+	        model.rightArmPose = _default.rightArmPose;
+	        model.leftArmPose = _default.leftArmPose;
+	        
+	        CompoundNBT compoundnbt = itemStack.getTagElement("displaymm");
+
+		      if(compoundnbt != null && compoundnbt.contains("MainColor", 99)) {
+		    	  //CompoundNBT maincompund = itemStack.getChildTag("MainColor");
+		    	  //armorcolorer = compoundnbt.getInt("MainColor");
+		    	  model.MainColor = compoundnbt.getInt("MainColor");
+
+		      }
+		      if(compoundnbt != null && compoundnbt.contains("SecondaryColor", 99)) {
+
+		    	  model.SecondaryColor = compoundnbt.getInt("SecondaryColor");
+
+		      }
+		      
+		      if(compoundnbt != null && compoundnbt.contains("WhiteColor", 99)) {
+
+		    	  model.WhiteColor = compoundnbt.getInt("WhiteColor");
+
+		      }
+		      
+		      if(compoundnbt != null && compoundnbt.contains("ThirdColor", 99)) {
+
+		    	  model.ThirdColor = compoundnbt.getInt("ThirdColor");
+
+		      }
+		      
+		      if(compoundnbt != null && compoundnbt.contains("GlowyColor", 99)) {
+
+		    	  model.GlowyColor = compoundnbt.getInt("GlowyColor");
+
+		      }
+		      
+		      if(compoundnbt != null && compoundnbt.contains("GrayColor", 99)) {
+
+		    	  model.GrayColor = compoundnbt.getInt("GrayColor");
+
+		      }
+		      if(compoundnbt != null && compoundnbt.contains("FourthColor", 99)) {
+
+		    	  model.FourthColor = compoundnbt.getInt("FourthColor");
+
+		      }
+		      if(compoundnbt != null && compoundnbt.contains("FifthColor", 99)) {
+
+		    	  model.FifthColor = compoundnbt.getInt("FifthColor");
+
+		      }
+	      
+			this.MainColor = model.MainColor;
+			this.SecondaryColor = model.SecondaryColor;
+			this.WhiteColor = model.WhiteColor;
+			this.ThirdColor = model.ThirdColor;
+			this.GlowyColor = model.GlowyColor;
+			this.GrayColor = model.GrayColor;
+
+	        return (A) model;
+			}
+        
+//		if(slot == EquipmentSlotType.HEAD) {
+//			protomanarmor_main model = new protomanarmor_main(1.0f);
+//			model.bipedHeadwear.showModel = armorSlot == EquipmentSlotType.HEAD;
+//		}
+//		if(slot == EquipmentSlotType.CHEST) {
+//			protomanarmor_chestplate modelchestplate = new protomanarmor_chestplate(1.0f);
+//			model.bipedHeadwear.showModel = armorSlot == EquipmentSlotType.CHEST;
+//
+//        modelchestplate.isChild = _default.isChild;
+//        modelchestplate.isSneak = _default.isSneak;
+//        modelchestplate.isSitting = _default.isSitting;
+//        modelchestplate.rightArmPose = _default.rightArmPose;
+//        modelchestplate.leftArmPose = _default.leftArmPose;
+//
+//        return (A) modelchestplate;
+//		}
+	}
+	
+//
+   // @Override
+    //public <A extends BipedModel<?>> A getArmorModel(LivingEntity entityLiving, ItemStack itemStack, EquipmentSlotType armorSlot, A _default) {
+    //    return null;
+    //}
+
+//	@Nullable
+    @Override
+    public String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlotType slot, String type) {
+        return "megamanarmormod:textures/armor/bit.png";
+    }
+	
+}
