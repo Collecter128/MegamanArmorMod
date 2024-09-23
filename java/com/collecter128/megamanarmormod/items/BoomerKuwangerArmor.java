d6652dce1024c5d495a563ff473cb5f94e5122ba
Collecter128 <45398071+Collecter128@users.noreply.github.com>
2024-09-23 10:29:53 -0400

    Add files via upload

---
+++ java/com/collecter128/megamanarmormod/items/BoomerKuwangerArmor.java
@@ -0,0 +1,209 @@
+package com.collecter128.megamanarmormod.items;
+
+import com.collecter128.megamanarmormod.client.models.boomerkuwanger;
+import com.collecter128.megamanarmormod.client.models.boomerkuwanger_leggings;
+
+import net.minecraft.client.renderer.entity.model.BipedModel;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.ArmorItem;
+import net.minecraft.item.IArmorMaterial;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.CompoundNBT;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+
+public class BoomerKuwangerArmor extends ArmorItem{
+	
+	public int MainColorDefault = 16725033;//Main Body Color Red
+	public int SecondaryColorDefault = 2706943;//Secondary Blue
+	public int ThirdColorDefault = 16767529; //Yellow parts
+	public int WhiteColorDefault = 16777215;//Basic White
+	public int GlowyColorDefault = 8247606;//Glow? Green
+	public int GrayColorDefault = 12237498;//gray
+	public int FourthColorDefault = 3096145; //Black
+	public int FifthColorDefault = 2729727; //Light Blue
+	public int MainColor = MainColorDefault;
+	public int SecondaryColor = SecondaryColorDefault;
+	public int ThirdColor = ThirdColorDefault;
+	public int WhiteColor = WhiteColorDefault;
+	public int GlowyColor = GlowyColorDefault;
+	public int GrayColor = GrayColorDefault;
+	public int FourthColor = FourthColorDefault;
+	public int FifthColor = FifthColorDefault;
+
+	public BoomerKuwangerArmor(IArmorMaterial materialIn, EquipmentSlotType slot, Properties builderIn) {
+		super(materialIn, slot, builderIn);
+		
+	}
+	@OnlyIn(Dist.CLIENT)
+	@Override
+	public <A extends BipedModel<?>> A getArmorModel(LivingEntity entityLiving, ItemStack itemStack, EquipmentSlotType armorSlot, A _default) {
+		
+		if(armorSlot != EquipmentSlotType.LEGS) {
+			boomerkuwanger model = new boomerkuwanger(1.0f, armorSlot);
+		model.hat.visible = armorSlot == EquipmentSlotType.HEAD;
+
+		model.young = _default.young;
+        model.crouching = _default.crouching;
+        model.riding = _default.riding;
+        model.rightArmPose = _default.rightArmPose;
+        model.leftArmPose = _default.leftArmPose;
+        
+        CompoundNBT compoundnbt = itemStack.getTagElement("displaymm");
+
+	      if(compoundnbt != null && compoundnbt.contains("MainColor", 99)) {
+	    	  //CompoundNBT maincompund = itemStack.getChildTag("MainColor");
+	    	  //armorcolorer = compoundnbt.getInt("MainColor");
+	    	  model.MainColor = compoundnbt.getInt("MainColor");
+
+	      }
+	      if(compoundnbt != null && compoundnbt.contains("SecondaryColor", 99)) {
+
+	    	  model.SecondaryColor = compoundnbt.getInt("SecondaryColor");
+
+	      }
+	      
+	      if(compoundnbt != null && compoundnbt.contains("WhiteColor", 99)) {
+
+	    	  model.WhiteColor = compoundnbt.getInt("WhiteColor");
+
+	      }
+	      
+	      if(compoundnbt != null && compoundnbt.contains("ThirdColor", 99)) {
+
+	    	  model.ThirdColor = compoundnbt.getInt("ThirdColor");
+
+	      }
+	      
+	      if(compoundnbt != null && compoundnbt.contains("GlowyColor", 99)) {
+
+	    	  model.GlowyColor = compoundnbt.getInt("GlowyColor");
+
+	      }
+	      
+	      if(compoundnbt != null && compoundnbt.contains("GrayColor", 99)) {
+
+	    	  model.GrayColor = compoundnbt.getInt("GrayColor");
+
+	      }
+	      if(compoundnbt != null && compoundnbt.contains("FourthColor", 99)) {
+
+	    	  model.FourthColor = compoundnbt.getInt("FourthColor");
+
+	      }
+	      if(compoundnbt != null && compoundnbt.contains("FifthColor", 99)) {
+
+	    	  model.FifthColor = compoundnbt.getInt("FifthColor");
+
+	      }
+      
+		this.MainColor = model.MainColor;
+		this.SecondaryColor = model.SecondaryColor;
+		this.WhiteColor = model.WhiteColor;
+		this.ThirdColor = model.ThirdColor;
+		this.GlowyColor = model.GlowyColor;
+		this.GrayColor = model.GrayColor;
+
+        return (A) model;
+		}
+		else {
+			boomerkuwanger_leggings model = new boomerkuwanger_leggings(1.0f);
+			model.hat.visible = armorSlot == EquipmentSlotType.HEAD;
+
+			model.young = _default.young;
+	        model.crouching = _default.crouching;
+	        model.riding = _default.riding;
+	        model.rightArmPose = _default.rightArmPose;
+	        model.leftArmPose = _default.leftArmPose;
+	        
+	        CompoundNBT compoundnbt = itemStack.getTagElement("displaymm");
+
+		      if(compoundnbt != null && compoundnbt.contains("MainColor", 99)) {
+		    	  //CompoundNBT maincompund = itemStack.getChildTag("MainColor");
+		    	  //armorcolorer = compoundnbt.getInt("MainColor");
+		    	  model.MainColor = compoundnbt.getInt("MainColor");
+
+		      }
+		      if(compoundnbt != null && compoundnbt.contains("SecondaryColor", 99)) {
+
+		    	  model.SecondaryColor = compoundnbt.getInt("SecondaryColor");
+
+		      }
+		      
+		      if(compoundnbt != null && compoundnbt.contains("WhiteColor", 99)) {
+
+		    	  model.WhiteColor = compoundnbt.getInt("WhiteColor");
+
+		      }
+		      
+		      if(compoundnbt != null && compoundnbt.contains("ThirdColor", 99)) {
+
+		    	  model.ThirdColor = compoundnbt.getInt("ThirdColor");
+
+		      }
+		      
+		      if(compoundnbt != null && compoundnbt.contains("GlowyColor", 99)) {
+
+		    	  model.GlowyColor = compoundnbt.getInt("GlowyColor");
+
+		      }
+		      
+		      if(compoundnbt != null && compoundnbt.contains("GrayColor", 99)) {
+
+		    	  model.GrayColor = compoundnbt.getInt("GrayColor");
+
+		      }
+		      if(compoundnbt != null && compoundnbt.contains("FourthColor", 99)) {
+
+		    	  model.FourthColor = compoundnbt.getInt("FourthColor");
+
+		      }
+		      if(compoundnbt != null && compoundnbt.contains("FifthColor", 99)) {
+
+		    	  model.FifthColor = compoundnbt.getInt("FifthColor");
+
+		      }
+	      
+			this.MainColor = model.MainColor;
+			this.SecondaryColor = model.SecondaryColor;
+			this.WhiteColor = model.WhiteColor;
+			this.ThirdColor = model.ThirdColor;
+			this.GlowyColor = model.GlowyColor;
+			this.GrayColor = model.GrayColor;
+
+	        return (A) model;
+			}
+        
+//		if(slot == EquipmentSlotType.HEAD) {
+//			protomanarmor_main model = new protomanarmor_main(1.0f);
+//			model.bipedHeadwear.showModel = armorSlot == EquipmentSlotType.HEAD;
+//		}
+//		if(slot == EquipmentSlotType.CHEST) {
+//			protomanarmor_chestplate modelchestplate = new protomanarmor_chestplate(1.0f);
+//			model.bipedHeadwear.showModel = armorSlot == EquipmentSlotType.CHEST;
+//
+//        modelchestplate.isChild = _default.isChild;
+//        modelchestplate.isSneak = _default.isSneak;
+//        modelchestplate.isSitting = _default.isSitting;
+//        modelchestplate.rightArmPose = _default.rightArmPose;
+//        modelchestplate.leftArmPose = _default.leftArmPose;
+//
+//        return (A) modelchestplate;
+//		}
+	}
+	
+//
+   // @Override
+    //public <A extends BipedModel<?>> A getArmorModel(LivingEntity entityLiving, ItemStack itemStack, EquipmentSlotType armorSlot, A _default) {
+    //    return null;
+    //}
+
+//	@Nullable
+    @Override
+    public String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlotType slot, String type) {
+        return "megamanarmormod:textures/armor/boomerkuwanger.png";
+    }
+	
+}
---
+++ java/com/collecter128/megamanarmormod/items/ServbotArmor.java
@@ -0,0 +1,204 @@
+package com.collecter128.megamanarmormod.items;
+
+import com.collecter128.megamanarmormod.client.models.servbot;
+import com.collecter128.megamanarmormod.client.models.servbot_leggings;
+
+import net.minecraft.client.renderer.entity.model.BipedModel;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.ArmorItem;
+import net.minecraft.item.IArmorMaterial;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.CompoundNBT;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+
+public class ServbotArmor extends ArmorItem{
+	
+	public int MainColorDefault = 2913020;//Main Body Color Dark Blue
+	public int SecondaryColorDefault = 9431294;//Secondary LightBlue
+	public int ThirdColorDefault = 16771123; //Yellow parts
+	public int WhiteColorDefault = 16777215;//Basic White
+	public int GlowyColorDefault = 16726579;//Glow? red not used
+	public int GrayColorDefault = 9737364;//Gray
+	public int FourthColorDefault = 9737364; //Not Used Gray 
+	public int FifthColorDefault = 16729897; //Red
+	public int MainColor = MainColorDefault;
+	public int SecondaryColor = SecondaryColorDefault;
+	public int ThirdColor = ThirdColorDefault;
+	public int WhiteColor = WhiteColorDefault;
+	public int GlowyColor = GlowyColorDefault;
+	public int GrayColor = GrayColorDefault;
+	public int FourthColor = FourthColorDefault;
+	public int FifthColor = FifthColorDefault;
+
+	public ServbotArmor(IArmorMaterial materialIn, EquipmentSlotType slot, Properties builderIn) {
+		super(materialIn, slot, builderIn);
+		
+	}
+	@OnlyIn(Dist.CLIENT)
+	@Override
+	public <A extends BipedModel<?>> A getArmorModel(LivingEntity entityLiving, ItemStack itemStack, EquipmentSlotType armorSlot, A _default) {
+		
+		if(armorSlot != EquipmentSlotType.LEGS) {
+			servbot model = new servbot(1.0f, armorSlot);
+		model.hat.visible = armorSlot == EquipmentSlotType.HEAD;
+
+		model.young = _default.young;
+        model.crouching = _default.crouching;
+        model.riding = _default.riding;
+        model.rightArmPose = _default.rightArmPose;
+        model.leftArmPose = _default.leftArmPose;
+        
+        CompoundNBT compoundnbt = itemStack.getTagElement("displaymm");
+
+	      if(compoundnbt != null && compoundnbt.contains("MainColor", 99)) {
+	    	  //CompoundNBT maincompund = itemStack.getChildTag("MainColor");
+	    	  //armorcolorer = compoundnbt.getInt("MainColor");
+	    	  model.MainColor = compoundnbt.getInt("MainColor");
+
+	      }
+	      if(compoundnbt != null && compoundnbt.contains("SecondaryColor", 99)) {
+
+	    	  model.SecondaryColor = compoundnbt.getInt("SecondaryColor");
+
+	      }
+	      
+	      if(compoundnbt != null && compoundnbt.contains("WhiteColor", 99)) {
+
+	    	  model.WhiteColor = compoundnbt.getInt("WhiteColor");
+
+	      }
+	      
+	      if(compoundnbt != null && compoundnbt.contains("ThirdColor", 99)) {
+
+	    	  model.ThirdColor = compoundnbt.getInt("ThirdColor");
+
+	      }
+	      
+	      if(compoundnbt != null && compoundnbt.contains("GlowyColor", 99)) {
+
+	    	  model.GlowyColor = compoundnbt.getInt("GlowyColor");
+
+	      }
+	      
+	      if(compoundnbt != null && compoundnbt.contains("GrayColor", 99)) {
+
+	    	  model.GrayColor = compoundnbt.getInt("GrayColor");
+
+	      }
+	      if(compoundnbt != null && compoundnbt.contains("FourthColor", 99)) {
+
+	    	  model.FourthColor = compoundnbt.getInt("FourthColor");
+
+	      }
+	      if(compoundnbt != null && compoundnbt.contains("FifthColor", 99)) {
+
+	    	  model.FifthColor = compoundnbt.getInt("FifthColor");
+
+	      }
+      
+		this.MainColor = model.MainColor;
+		this.SecondaryColor = model.SecondaryColor;
+		this.WhiteColor = model.WhiteColor;
+		this.ThirdColor = model.ThirdColor;
+		this.GlowyColor = model.GlowyColor;
+		this.GrayColor = model.GrayColor;
+
+        return (A) model;
+		}
+		else {
+			servbot_leggings model = new servbot_leggings(1.0f);
+			model.hat.visible = armorSlot == EquipmentSlotType.HEAD;
+
+			model.young = _default.young;
+	        model.crouching = _default.crouching;
+	        model.riding = _default.riding;
+	        model.rightArmPose = _default.rightArmPose;
+	        model.leftArmPose = _default.leftArmPose;
+	        
+	        CompoundNBT compoundnbt = itemStack.getTagElement("displaymm");
+
+		      if(compoundnbt != null && compoundnbt.contains("MainColor", 99)) {
+		    	  //CompoundNBT maincompund = itemStack.getChildTag("MainColor");
+		    	  //armorcolorer = compoundnbt.getInt("MainColor");
+		    	  model.MainColor = compoundnbt.getInt("MainColor");
+
+		      }
+		      if(compoundnbt != null && compoundnbt.contains("SecondaryColor", 99)) {
+
+		    	  model.SecondaryColor = compoundnbt.getInt("SecondaryColor");
+
+		      }
+		      
+		      if(compoundnbt != null && compoundnbt.contains("WhiteColor", 99)) {
+
+		    	  model.WhiteColor = compoundnbt.getInt("WhiteColor");
+
+		      }
+		      
+		      if(compoundnbt != null && compoundnbt.contains("ThirdColor", 99)) {
+
+		    	  model.ThirdColor = compoundnbt.getInt("ThirdColor");
+
+		      }
+		      
+		      if(compoundnbt != null && compoundnbt.contains("GlowyColor", 99)) {
+
+		    	  model.GlowyColor = compoundnbt.getInt("GlowyColor");
+
+		      }
+		      
+		      if(compoundnbt != null && compoundnbt.contains("GrayColor", 99)) {
+
+		    	  model.GrayColor = compoundnbt.getInt("GrayColor");
+
+		      }
+		      if(compoundnbt != null && compoundnbt.contains("FifthColor", 99)) {
+
+		    	  model.FifthColor = compoundnbt.getInt("FifthColor");
+
+		      }
+	      
+			this.MainColor = model.MainColor;
+			this.SecondaryColor = model.SecondaryColor;
+			this.WhiteColor = model.WhiteColor;
+			this.ThirdColor = model.ThirdColor;
+			this.GlowyColor = model.GlowyColor;
+			this.GrayColor = model.GrayColor;
+
+	        return (A) model;
+			}
+        
+//		if(slot == EquipmentSlotType.HEAD) {
+//			protomanarmor_main model = new protomanarmor_main(1.0f);
+//			model.bipedHeadwear.showModel = armorSlot == EquipmentSlotType.HEAD;
+//		}
+//		if(slot == EquipmentSlotType.CHEST) {
+//			protomanarmor_chestplate modelchestplate = new protomanarmor_chestplate(1.0f);
+//			model.bipedHeadwear.showModel = armorSlot == EquipmentSlotType.CHEST;
+//
+//        modelchestplate.isChild = _default.isChild;
+//        modelchestplate.isSneak = _default.isSneak;
+//        modelchestplate.isSitting = _default.isSitting;
+//        modelchestplate.rightArmPose = _default.rightArmPose;
+//        modelchestplate.leftArmPose = _default.leftArmPose;
+//
+//        return (A) modelchestplate;
+//		}
+	}
+	
+//
+   // @Override
+    //public <A extends BipedModel<?>> A getArmorModel(LivingEntity entityLiving, ItemStack itemStack, EquipmentSlotType armorSlot, A _default) {
+    //    return null;
+    //}
+
+//	@Nullable
+    @Override
+    public String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlotType slot, String type) {
+        return "megamanarmormod:textures/armor/servbot.png";
+    }
+	
+}
